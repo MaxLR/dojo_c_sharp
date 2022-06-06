2fd838ac1ff27986fccc5da027bd955bc35457f8
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-06-06 12:32:59 -0400

    SLL Notes

---
+++ algos/data_structures/SinglyLinkedList/notes.js
@@ -0,0 +1,15 @@
+const node1 = { data: 1, next: null};
+const node2 = { data: 2, next: null};
+const node3 = { data: 3, next: null};
+
+node1.next = node2;
+node2.next = node3;
+
+// console.log(node1);
+
+let currentNode = node1;
+
+while(currentNode != null) {
+    console.log(currentNode.data);
+    currentNode = currentNode.next;
+}
