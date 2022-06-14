12e0752f91b37c3940a3ec01082ac29a128dd510
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-06-14 19:20:53 -0400

    Added the UD Functionality

--- ORM/Controllers/PostsController.cs
+++ ORM/Controllers/PostsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ORM.Models;
 
+
 public class PostsController : Controller
 {
     private ORMContext _context;
@@ -40,5 +41,71 @@ public class PostsController : Controller
         List<Post> allPosts = _context.Posts.ToList();
 
         return View("All", allPosts);
+    }
+
+    [HttpGet("/posts/{postID}")]
+    public IActionResult Details(int postID)
+    {
+        Post? post = _context.Posts.FirstOrDefault(post => post.PostId == postID);
+
+        if (post == null)
+        {
+            return RedirectToAction("All");
+        }
+
+        return View(post);
+    }
+
+    [HttpPost("/posts/delete/{postId}")]
+    public IActionResult Delete(int postId)
+    {
+        Post? post = _context.Posts.FirstOrDefault(post => post.PostId == postId);
+
+        if (post != null)
+        {
+            _context.Posts.Remove(post);
+            _context.SaveChanges();
+        }
+
+        return RedirectToAction("All");
+    }
+
+    [HttpGet("/posts/{postId}/edit")]
+    public IActionResult Edit(int postId)
+    {
+        Post? post = _context.Posts.FirstOrDefault(post => post.PostId == postId);
+
+        if (post != null)
+        {
+            return View("Edit", post);
+        }
+
+        return RedirectToAction("All");
+    }
+
+    [HttpPost("/posts/{postId}/update")]
+    public IActionResult Update(Post editedPost, int postId)
+    {
+        if (ModelState.IsValid)
+        {
+            Post? dbPost = _context.Posts.FirstOrDefault(post => post.PostId == postId);
+
+            if (dbPost != null)
+            {
+                dbPost.Topic = editedPost.Topic;
+                dbPost.Body = editedPost.Body;
+                dbPost.ImgUrl = editedPost.ImgUrl;
+                dbPost.UpdatedAt = DateTime.Now;
+
+                _context.Posts.Update(dbPost);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Details", postId);
+        }
+        else
+        {
+            return Edit(postId);
+        }
     }
 }
