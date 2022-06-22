b5c342324d7abee71495a316cd079ed79565d41d
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-06-22 11:55:55 -0400

    W3D3 Algos

--- algos/data_structures/DoublyLinkedList/index.js
+++ algos/data_structures/DoublyLinkedList/index.js
@@ -4,82 +4,166 @@ and the empty methods below the constructor.
 */
 
 /**
+ * Class to represent a Node for a DoublyLinkedList.
+ */
+ class DLLNode {
+  /**
+   * Executed when the new keyword is used to construct a new node instance.
+   * @param {any} data The data the new node will store.
+   */
+  constructor(data) {
+    this.data = data;
+    /**
+     * By default a new node instance will not be connected to any other nodes,
+     * these properties will be set after instantiation to connect the node to
+     * a list. However, the head prev should remain null.
+     *
+     * @type {DLLNode|null}
+     */
+    this.prev = null;
+    /** @type {DLLNode|null} */
+    this.next = null;
+  }
+}
+
+/**
  * A class to represent a doubly linked list and contain all of it's methods.
  * A doubly linked list is a singly linked list that can be traversed in both
  * directions.
  */
- class DoublyLinkedList {
-    /**
-     * Executed when the new keyword is used to construct a new DoublyLInkedList
-     * instance that inherits these methods and properties.
-     */
-    constructor() {
-      // TODO: implement the constructor.
+class DoublyLinkedList {
+  /**
+   * Executed when the new keyword is used to construct a new DoublyLInkedList
+   * instance that inherits these methods and properties.
+   */
+  constructor() {
+    // The list is empty to start.
+    /** @type {DLLNode|null} */
+    this.head = null;
+    /** @type {DLLNode|null} */
+    this.tail = null;
+  }
+  
+  /**
+   * Creates a new node and adds it at the front of this list.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @param {any} data The data for the new node.
+   * @returns {DoublyLinkedList} This list.
+  */
+  insertAtFront(data) {
+    const newHead = new DLLNode(data);
+
+    if (this.isEmpty()) {
+      this.head = newHead;
+      this.tail = newHead;
+    } else {
+      this.head.prev = newHead;
+      newHead.next = this.head;
+      this.head = newHead;
     }
-  
-    /**
-     * Creates a new node and adds it at the front of this list.
-     * - Time: O(?).
-     * - Space: O(?).
-     * @param {any} data The data for the new node.
-     * @returns {DoublyLinkedList} This list.
-     */
-    insertAtFront(data) {}
-  
-    /**
-     * Creates a new node and adds it at the back of this list.
-     * - Time: O(?).
-     * - Space: O(?).
-     * @param {any} data The data for the new node.
-     * @returns {DoublyLinkedList} This list.
-     */
-    insertAtBack(data) {}
-  
-    // EXTRA
-    /**
-     * Removes the middle node in this list.
-     * - Time: O(?).
-     * - Space: O(?).
-     * @returns {any} The data of the removed node.
-     */
-    removeMiddleNode() {}
-  
-    /**
-     * Determines if this list is empty.
-     * - Time: O(1) constant.
-     * - Space: O(1) constant.
-     * @returns {boolean} Indicates if this list is empty.
-     */
-    isEmpty() {
-      return this.head === null;
+    return this;
+  }
+
+  /**
+   * Creates a new node and adds it at the back of this list.
+   * - Time: O(1) constant. No loop is needed since we have direct access to
+   *    the tail.
+   * - Space: O(1) constant.
+   * @param {any} data The data for the new node.
+   * @returns {DoublyLinkedList} This list.
+   */
+  insertAtBack(data) {
+    const newTail = new DLLNode(data);
+
+    if (this.isEmpty()) {
+      // if no head set the newTail to be both the head and the tail
+      this.head = newTail;
+      this.tail = newTail;
+    } else {
+      this.tail.next = newTail;
+      newTail.prev = this.tail;
+
+      this.tail = newTail;
     }
-  
-    /**
-     * Converts this list to an array of the node's data.
-     * - Time: O(n) linear, n = list length.
-     * - Space: O(n) linear, array grows as list length increases.
-     * @returns {Array<any>} All the data of the nodes.
-     */
-    toArray() {
-      const vals = [];
-      let runner = this.head;
-  
-      while (runner) {
-        vals.push(runner.data);
-        runner = runner.next;
+    return this;
+  }
+
+  /**
+   * Removes the middle node in this list.
+   * - Time: O(0.5n) -> O(n) linear, n = list length.
+   *    Since it's not constant we simplify it to O(n). Without the early
+   *    exists, it would not be 0.5n.
+   * - Space: O(1) constant.
+   * @returns {any} The data of the removed node.
+   */
+  removeMiddleNode() {
+    // when there is only 1 node, it is the middle, remove it.
+    if (!this.isEmpty() && this.head === this.tail) {
+      const removedData = this.head.data;
+      this.head = null;
+      this.tail = null;
+      return removedData;
+    }
+
+    let forwardRunner = this.head;
+    let backwardsRunner = this.tail;
+
+    while (forwardRunner && backwardsRunner) {
+      if (forwardRunner === backwardsRunner) {
+        const midNode = forwardRunner;
+        midNode.prev.next = midNode.next;
+        midNode.next.prev = midNode.prev;
+        return midNode.data;
       }
-      return vals;
+
+      // runners passed each other without stopping on the same node, even length, we can exit early
+      if (forwardRunner.prev === backwardsRunner) {
+        return null;
+      }
+
+      forwardRunner = forwardRunner.next;
+      backwardsRunner = backwardsRunner.prev;
     }
-  
-    /**
-     * Adds all the given items to the back of this list.
-     * @param {Array<any>} items Items to be added to the back of this list.
-     * @returns {DoublyLinkedList} This list.
-     */
-    insertAtBackMany(items = []) {
-      items.forEach((item) => this.insertAtBack(item));
-      return this;
+    return null;
+  }
+
+  /**
+   * Determines if this list is empty.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {boolean} Indicates if this list is empty.
+   */
+  isEmpty() {
+    return this.head === null;
+  }
+
+  /**
+   * Converts this list to an array of the node's data.
+   * - Time: O(n) linear, n = list length.
+   * - Space: O(n) linear, array grows as list length increases.
+   * @returns {Array<any>} All the data of the nodes.
+   */
+  toArray() {
+    const vals = [];
+    let runner = this.head;
+
+    while (runner) {
+      vals.push(runner.data);
+      runner = runner.next;
     }
+    return vals;
+  }
+
+  /**
+   * Adds all the given items to the back of this list.
+   * @param {Array<any>} items Items to be added to the back of this list.
+   * @returns {DoublyLinkedList} This list.
+   */
+  insertAtBackMany(items = []) {
+    items.forEach((item) => this.insertAtBack(item));
+    return this;
+  }
   }
   
   const emptyList = new DoublyLinkedList();
--- algos/data_structures/SinglyLinkedList/index.js
+++ algos/data_structures/SinglyLinkedList/index.js
@@ -42,6 +42,32 @@
     }
 
     /**
+     * Reverses this list in-place without using any extra lists.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {SinglyLinkedList} This list.
+     */
+    reverse() {}
+
+    /**
+     * Determines whether the list has a loop in it which would result in
+     * infinitely traversing unless otherwise avoided. A loop is when a node's
+     * next points to a node that is behind it.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {boolean} Whether the list has a loop or not.
+     */
+    hasLoop() {}
+
+    /**
+     * Removes all the nodes that have a negative integer as their data.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {SinglyLinkedList} This list after the negatives are removed.
+     */
+    removeNegatives() {}
+
+    /**
      * Concatenates the nodes of a given list onto the back of this list.
      * - Time: O(?).
      * - Space: O(?).
