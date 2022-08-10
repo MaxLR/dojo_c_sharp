ee7bef098ce9657bd87fe76bb0468f82ca56d600
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-08-10 10:30:40 -0400

    W2D3 Algos

--- algos/data_structures/BinarySearchTrees/index.js
+++ algos/data_structures/BinarySearchTrees/index.js
@@ -39,32 +39,91 @@ class BinarySearchTree {
     }
 
     /**
-     * Determines if this tree contains the given searchVal.
+     * Inserts a new node with the given newVal in the right place to preserver
+     * the order of this tree.
      * - Time: O(?).
      * - Space: O(?).
-     * @param {number} searchVal The number to search for in the node's data.
-     * @returns {boolean} Indicates if the searchVal was found.
+     * @param {number} newVal The data to be added to a new node.
+     * @returns {BinarySearchTree} This tree.
      */
-    contains(searchVal) {}
+    insert(newVal) {}
+
+    /**
+     * Inserts a new node with the given newVal in the right place to preserver
+     * the order of this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {number} newVal The data to be added to a new node.
+     * @param {Node} curr The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {BinarySearchTree} This tree.
+     */
+    insertRecursive(newVal, curr = this.root) {}
 
     /**
      * Determines if this tree contains the given searchVal.
-     * - Time: O(?).
-     * - Space: O(?).
+     * - Time: O(h) linear, h = height of tree.
+     * - Space: O(1) constant.
      * @param {number} searchVal The number to search for in the node's data.
      * @returns {boolean} Indicates if the searchVal was found.
      */
-    containsRecursive(searchVal, current = this.root) {}
+    contains(searchVal) {
+      let current = this.root;
+
+      while (current) {
+        if (current.data === searchVal) {
+          return true;
+        }
+
+        if (searchVal < current.data) {
+          current = current.left;
+        } else {
+          current = current.right;
+        }
+      }
+      return false;
+    }
+
+    /**
+     * Determines if this tree contains the given searchVal.
+     * - Time: O(h) linear, h = height of tree.
+     * - Space: O(h) linear due to the call stack.
+     * @param {number} searchVal The number to search for in the node's data.
+     * @returns {boolean} Indicates if the searchVal was found.
+     */
+    containsRecursive(searchVal, current = this.root) {
+      if (current === null) {
+        return false;
+      }
+
+      if (current.data === searchVal) {
+        return true;
+      }
+
+      if (searchVal < current.data) {
+        return this.containsRecursive(searchVal, current.left);
+      }
+
+      if (searchVal > current.data) {
+        return this.containsRecursive(searchVal, current.right);
+      }
+    }
 
     /**
      * Calculates the range (max - min) from the given startNode.
-     * - Time: O(?).
-     * - Space: O(?).
-     * @param {Node} startNode The node to start from to calculate the range.
+     * - Time: O(rightHeight + leftHeight) -> still linear so simplify to O(h).
+     * - Space: O(h) linear due to the call stack. The max side finishes before
+     *    the right min side is added to the stack.
+     * @param {BSTNode} startNode The node to start from to calculate the range.
      * @returns {number|null} The range of this tree or a sub tree depending on if the
      *    startNode is the root or not.
      */
-    range(startNode = this.root) {}
+    range(startNode = this.root) {
+      if (!startNode) {
+        return null;
+      }
+      return this.max(startNode) - this.min(startNode);
+    }
   
     /**
      * Determines if this tree is empty.
