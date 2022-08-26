213b91d8616937d6dae0e5b92f95c75de6ef574a
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-08-26 10:29:52 -0400

    W4D5 Algos

---
+++ algos/data_structures/BinaryHeap/ordermaximizer.js
@@ -0,0 +1,149 @@
+/* 
+Design a class to process incoming orders and keep track of the running total
+sales and leftover orders awaiting new orders to possibly match with.
+Orders should be matched and processed based on the following criteria:
+  - Match the highest available buy price to the lowest available sell price
+  - Only process the order if the sell price is <= the buy price.
+  - Processing a matched pair of orders simply means using up available quantity
+  and incrementing the total by the sale amount.
+  - Continue matching orders until there are no more viable matches.
+  
+Leftover orders / quantity that couldn't be matched should be kept so they can
+be matched when new orders come in.
+*/
+
+/**
+ * @typedef {object} Order
+ * @property {('buy' | 'sell')} action
+ * @property {number} price
+ * @property {number} quantity
+ */
+
+/** @typedef {Order & { action: 'sell'}} SellOrder */
+/** @typedef {Order & { action: 'buy'}} BuyOrder */
+
+/** @type {Order[]} */
+const incomingOrders1 = [
+    {
+      price: 155,
+      quantity: 3,
+      action: "buy",
+    },
+    {
+      price: 100,
+      quantity: 5,
+      action: "sell",
+    },
+    {
+      price: 125,
+      quantity: 3,
+      action: "buy",
+    },
+    {
+      price: 90,
+      quantity: 2,
+      action: "buy",
+    },
+    {
+      price: 110,
+      quantity: 4,
+      action: "sell",
+    },
+  ];
+  
+  /* 
+  Explanation:
+  The highest buy price of 155 is matched to the lowest sell price of 100.
+  3 are bought at the 100 sell price, leaving 2 more sells left at that price.
+  total: 300 (3 * 100)
+  The next highest buy price of 125 is matched to the 2 leftover sells at 100,
+  increasing the total by 200 for a total of 500.
+  There is 1 more buy at 125 leftover which is matched to the next lowest sell
+  at a price of 110 increasing the total by 110 for a total of 610.
+  There are remaining buys at price 90 but the price is below the 3 leftover
+  sells at 110 so no more orders can be processed.
+  The total is 610 now until a new batch of orders comes in and are processed
+  with each other and the remaining orders that haven't been matched.
+  */
+  const expected1 = 610;
+  
+  /** @type {Order[]} */
+  const incomingOrders2 = [
+    {
+      price: 80,
+      quantity: 6,
+      action: "buy",
+    },
+    {
+      price: 120,
+      quantity: 4,
+      action: "sell",
+    },
+    {
+      price: 80,
+      quantity: 8,
+      action: "sell",
+    },
+  ];
+  
+  /* 
+  Explanation: when these orders are added to the same OrderSaleMaximizer
+  instance, they will be merged with the leftover orders and the total will
+  continue to be incremented.
+  1. the 80 priced sell will be matched with the 100 price buys:
+    - total: 610 + (80 * 2) = 770
+    - the 80 sell has 6 quantity left
+  2. the remaining 80 sell quantities are matched with the next highest buy which
+      is the leftover 90 price buys:
+    - total: 770 + (80 * 2) = 930
+    - the 80 sell has 4 quantity left
+  3. the remaining 80 price sell quantities match with the next highest buys at:
+    price 80
+    - total: 930 + (80 * 4) = 1250
+    - the 80 price sell has no more quantity left
+    - the 80 price buy has 2 quantity left
+    - there are no more sells prices <= any buy prices
+  */
+  const expected2 = 1250;
+  
+  // Design a class below to handle the above requirements.
+  
+  /*****************************************************************************/
+  
+  /* 
+  Thought process:
+  Notice the smallest sell is needed to be matched to the largest buy
+  and then the next smallest sell needs to be matched to the next largest buy.
+  Whenever you need to repeatedly get the smallest or the largest from a set
+  of numbers or items, a MinHeap (smallest) and / or MaxHeap (largest)
+  are useful because they are built to efficiently handle that need.
+  Realizing the need for a MinHeap for sells and a MaxHeap for buys is already a
+  big step towards solving this algorithm because it clarifies the main problem
+  of how to match a sell to a buy.
+  Next it's  pretty straightforward to decide to add all the orders we are
+  given into the heaps so they are ready to be extracted in the correct order,
+  insert sells into a MinHeap and buys into a MaxHeap.
+  Doing this as step 1 also already solves the issue of having left over orders
+  that found no match, because the next batch of orders will be added into the
+  heaps that may contain old orders from the prior batch and the heap will
+  sort them.
+  After we have all the new orders added to our heaps, all that is left is
+  straightforward logic to continuously (while) get the smallest sell and
+  largest buy if the heaps aren't empty and check if
+  the sell is <= the buy price and then compare and adjust the quantities
+  and tally the total sale price.
+  Lastly, extract any orders that run out of quantity.
+  */
+  
+  const { BinaryHeap } = require("../BinaryHeap");
+  
+  class OrderSaleMaximizer {
+    constructor() {
+        
+    }
+  
+}
+  
+
+
+module.exports = { OrderSaleMaximizer };
--- algos/data_structures/Queue/index.js
+++ algos/data_structures/Queue/index.js
@@ -9,18 +9,71 @@ class Queue {
     }
 
     /**
+     * Determines whether the sum of the left half of the queue items is equal to
+     * the sum of the right half. Avoid indexing the queue items directly via
+     * bracket notation, use the queue methods instead for practice.
+     * Use no extra array or objects.
+     * The queue should be returned to it's original order when done.
+     * - Time: O(n).
+     * - Space: O(1).
+     * @returns {boolean} Whether the sum of the left and right halves is equal.
+    */
+    isSumOfHalvesEqual() {
+      if (this.isEmpty() || this.items.length == 1) return true
+      let leftSum = 0
+      let rightSum = 0
+      const queueLength = this.size()
+
+      for(let i = 0; i < queueLength; i++) {
+        let tempVal = this.dequeue()
+
+        if (i < Math.floor(queueLength / 2)) {
+          leftSum += tempVal
+        } else if (i >= Math.ceil(queueLength / 2)) {
+          rightSum += tempVal
+        }
+
+        this.enqueue(tempVal)
+      }
+
+      return leftSum == rightSum
+    }
+
+    /**
      * Compares this queue to another given queue to see if they are equal.
      * Avoid indexing the queue items directly via bracket notation, use the
      * queue methods instead for practice.
      * Use no extra array or objects.
      * The queues should be returned to their original order when done.
-     * - Time: O(?).
-     * - Space: O(?).
+     * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+     *     array queue being O(n).
+     * - Space: O(1) constant.
      * @param {Queue} q2 The queue to be compared against this queue.
      * @returns {boolean} Whether all the items of the two queues are equal and
      *    in the same order.
      */
-    compareQueues(q2) {}
+    compareQueues(q2) {
+      if (this.size() !== q2.size()) {
+        return false;
+      }
+      let count = 0;
+      let isEqual = true;
+      const len = this.size();
+
+      while (count < len) {
+        const dequeued1 = this.dequeue();
+        const dequeued2 = q2.dequeue();
+
+        if (dequeued1 !== dequeued2) {
+          isEqual = false;
+        }
+
+        this.enqueue(dequeued1);
+        q2.enqueue(dequeued2);
+        count++;
+      }
+      return isEqual;
+    }
 
     /**
      * Determines if the queue is a palindrome (same items forward and backwards).
@@ -28,11 +81,36 @@ class Queue {
      * queue methods for practice.
      * Use only 1 stack as additional storage, no other arrays or objects.
      * The queue should be returned to its original order when done.
-     * - Time: O(?).
-     * - Space: O(?).
+     * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+     *     array queue being O(n).
+     * - Space: O(n) from the stack being used to store the items again.
      * @returns {boolean}
      */
-    isPalindrome() {}
+    isPalindrome() {
+      let isPalin = true;
+      const stack = new Stack(),
+        len = this.size();
+
+      for (let i = 0; i < len; i++) {
+        let dequeued = this.dequeue();
+        stack.push(dequeued);
+        // add it back so the queue items and order is restored at the end
+        this.enqueue(dequeued);
+      }
+
+      for (let i = 0; i < len; i++) {
+        let dequeued = this.dequeue();
+        let popped = stack.pop();
+
+        if (popped !== dequeued) {
+          isPalin = false;
+        }
+
+        // add it back so the queue items and order is restored at the end
+        this.enqueue(dequeued);
+      }
+      return isPalin;
+    }
   
     /**
      * Adds a new given item to the back of this queue.
@@ -87,3 +165,21 @@ class Queue {
       return this.items.length;
     }
 }
+
+
+let qa = new Queue()
+let qb = new Queue()
+
+qa.enqueue(1)
+qa.enqueue(3)
+qa.enqueue(5)
+
+qa.enqueue(5)
+
+qa.enqueue(1)
+qa.enqueue(1)
+qa.enqueue(3)
+
+console.log(qa.isSumOfHalvesEqual(qb))
+
+console.log(qa.items)
