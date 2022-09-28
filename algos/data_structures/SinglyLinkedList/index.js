8d879c485ee322a7549c69bb8fd6157118a6f8c8
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-09-28 11:29:47 -0400

    Updated Styling

--- algos/data_structures/SinglyLinkedList/index.js
+++ algos/data_structures/SinglyLinkedList/index.js
@@ -232,32 +232,32 @@
     recursiveMax(runner = this.head, maxNode = this.head) {}
 }
   
-  /******************************************************************* 
-  Multiple test lists already constructed to test your methods on.
-  Below commented code depends on insertAtBack method to be completed,
-  after completing it, uncomment the code.
-  */
-  const emptyList = new SinglyLinkedList();
-  
-  // const singleNodeList = new SinglyLinkedList().insertAtBackMany([1]);
-  // const biNodeList = new SinglyLinkedList().insertAtBackMany([1, 2]);
-  // const firstThreeList = new SinglyLinkedList().insertAtBackMany([1, 2, 3]);
-  // const secondThreeList = new SinglyLinkedList().insertAtBackMany([4, 5, 6]);
-  // const unorderedList = new SinglyLinkedList().insertAtBackMany([
-  //   -5, -10, 4, -3, 6, 1, -7, -2,
-  // ]);
-  
-  /* node 4 connects to node 1, back to head */
-  // const perfectLoopList = new SinglyLinkedList().insertAtBackMany([1, 2, 3, 4]);
-  // perfectLoopList.head.next.next.next = perfectLoopList.head;
-  
-  /* node 4 connects to node 2 */
-  // const loopList = new SinglyLinkedList().insertAtBackMany([1, 2, 3, 4]);
-  // loopList.head.next.next.next = loopList.head.next;
-  
-  // const sortedDupeList = new SinglyLinkedList().insertAtBackMany([
-  //   1, 1, 1, 2, 3, 3, 4, 5, 5,
-  // ]);
-  
-  // Print your list like so:
-  // console.log(firstThreeList.toArr());
+/******************************************************************* 
+Multiple test lists already constructed to test your methods on.
+Below commented code depends on insertAtBack method to be completed,
+after completing it, uncomment the code.
+*/
+const emptyList = new SinglyLinkedList();
+
+// const singleNodeList = new SinglyLinkedList().insertAtBackMany([1]);
+// const biNodeList = new SinglyLinkedList().insertAtBackMany([1, 2]);
+// const firstThreeList = new SinglyLinkedList().insertAtBackMany([1, 2, 3]);
+// const secondThreeList = new SinglyLinkedList().insertAtBackMany([4, 5, 6]);
+// const unorderedList = new SinglyLinkedList().insertAtBackMany([
+//   -5, -10, 4, -3, 6, 1, -7, -2,
+// ]);
+
+/* node 4 connects to node 1, back to head */
+// const perfectLoopList = new SinglyLinkedList().insertAtBackMany([1, 2, 3, 4]);
+// perfectLoopList.head.next.next.next = perfectLoopList.head;
+
+/* node 4 connects to node 2 */
+// const loopList = new SinglyLinkedList().insertAtBackMany([1, 2, 3, 4]);
+// loopList.head.next.next.next = loopList.head.next;
+
+// const sortedDupeList = new SinglyLinkedList().insertAtBackMany([
+//   1, 1, 1, 2, 3, 3, 4, 5, 5,
+// ]);
+
+// Print your list like so:
+// console.log(firstThreeList.toArr());
