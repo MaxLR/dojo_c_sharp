ebb30d1f98208b976b76efff6a1806d063f3e537
Max Rauchman <Max.L.Rauchman@gmail.com>
2022-10-03 11:15:23 -0400

    Formatting + Print Statement

--- algos/data_structures/BinarySearchTree/index.js
+++ algos/data_structures/BinarySearchTree/index.js
@@ -1,7 +1,7 @@
 /**
  * Class to represent a Node in a Binary Search Tree (BST).
  */
- class BSTNode {
+class BSTNode {
     /**
      * Constructs a new instance of a BST node.
      * @param {number} data The integer to store in the node.
@@ -21,13 +21,13 @@
       /** @type {BSTNode|null} */
       this.right = null;
     }
-  }
+}
   
   /**
    * Represents an ordered tree of nodes where the data of left nodes are <= to
    * their parent and the data of nodes to the right are > their parent's data.
    */
-  class BinarySearchTree {
+class BinarySearchTree {
     constructor() {
       /**
        * Just like the head of a linked list, this is the start of our tree which
@@ -102,39 +102,43 @@
   
       this.print(node.left, spaceCnt);
     }
-  }
+}
   
-  const emptyTree = new BinarySearchTree();
-  const oneNodeTree = new BinarySearchTree();
-  oneNodeTree.root = new BSTNode(10);
+const emptyTree = new BinarySearchTree();
+const oneNodeTree = new BinarySearchTree();
+oneNodeTree.root = new BSTNode(10);
   
   /* twoLevelTree
           root
           10
         /   \
-      5     15
+       5     15
   */
-  const twoLevelTree = new BinarySearchTree();
-  twoLevelTree.root = new BSTNode(10);
-  twoLevelTree.root.left = new BSTNode(5);
-  twoLevelTree.root.right = new BSTNode(15);
+const twoLevelTree = new BinarySearchTree();
+twoLevelTree.root = new BSTNode(10);
+twoLevelTree.root.left = new BSTNode(5);
+twoLevelTree.root.right = new BSTNode(15);
   
   /* threeLevelTree 
           root
           10
         /   \
       5     15
-    / \    / \
-  2   6  13  
+    / \    /  \
+   2   6  13  
   */
-  const threeLevelTree = new BinarySearchTree();
-  threeLevelTree.root = new BSTNode(10);
-  threeLevelTree.root.left = new BSTNode(5);
-  threeLevelTree.root.left.left = new BSTNode(2);
-  threeLevelTree.root.left.right = new BSTNode(6);
-  threeLevelTree.root.right = new BSTNode(15);
-  threeLevelTree.root.right.left = new BSTNode(13);
-  
+const threeLevelTree = new BinarySearchTree();
+threeLevelTree.root = new BSTNode(10);
+threeLevelTree.root.left = new BSTNode(5);
+threeLevelTree.root.left.left = new BSTNode(2);
+threeLevelTree.root.left.right = new BSTNode(6);
+threeLevelTree.root.right = new BSTNode(15);
+threeLevelTree.root.right.left = new BSTNode(13);
+
+// sample print statement
+threeLevelTree.print()
+
+
   /* fullTree
                       root
                   <-- 25 -->
