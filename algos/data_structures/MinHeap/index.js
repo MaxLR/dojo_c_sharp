a3248fa7a6e706d5c6df5f6372aba7df3bceacf7
Max R <max.l.rauchman@gmail.com>
2022-10-12 10:27:25 -0400

    W3D3 Algos

--- algos/data_structures/MinHeap/index.js
+++ algos/data_structures/MinHeap/index.js
@@ -8,133 +8,190 @@
  * - right child is located at: i * 2 + 1
  */
 class MinHeap {
-    constructor() {
-      /**
-       * 0th index not used, so null is a placeholder. Not using 0th index makes
-       * calculating the left and right children's index cleaner.
-       * This also effectively makes our array start at index 1.
-       */
-      this.heap = [null];
-    }
-
+  constructor() {
     /**
-     * @param {number} i
+     * 0th index not used, so null is a placeholder. Not using 0th index makes
+     * calculating the left and right children's index cleaner.
+     * This also effectively makes our array start at index 1.
      */
-    idxOfParent(i) {
-      return Math.floor(i / 2);
-    }
+    this.heap = [null];
+  }
 
-    /**
-     * @param {number} i
-     */
-    idxOfLeftChild(i) {
-      return i * 2;
-    }
+  /**
+   * @param {number} i
+   */
+  idxOfParent(i) {
+    return Math.floor(i / 2);
+  }
 
-    /**
-     * @param {number} i
-     */
-    idxOfRightChild(i) {
-      return i * 2 + 1;
-    }
+  /**
+   * @param {number} i
+   */
+  idxOfLeftChild(i) {
+    return i * 2;
+  }
 
-    /**
-     * Swaps two nodes.
-     * @param {number} i
-     * @param {number} j
-     */
-    swap(i, j) {
-      [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
-    }
+  /**
+   * @param {number} i
+   */
+  idxOfRightChild(i) {
+    return i * 2 + 1;
+  }
 
-    /**
-     * Retrieves the size of the heap, ignoring the null placeholder.
-     * - Time: O(1) constant.
-     * - Space: O(1) constant.
-     * @returns {number}
-     */
-    size() {
-      // - 1 since 0 index is unused
-      return this.heap.length - 1;
-    }
+  /**
+   * Swaps two nodes.
+   * @param {number} i
+   * @param {number} j
+   */
+  swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
 
-    /**
-     * Retrieves the top (minimum number) in the heap without removing it.
-     * - Time: O(1) constant.
-     * - Space: O(1) constant.
-     * @returns {?number} Null if empty.
-     */
-    top() {
-      return this.heap.length > 1 ? this.heap[1] : null;
-    }
-  
-    /**
-     * Inserts a new number into the heap and reorders heap to maintain order.
-     * 1. Push new num to back.
-     * 2. Iteratively swap the new num with it's parent while it is smaller than
-     *    it's parent.
-     * - Time: O(log n) logarithmic due to shiftUp.
-     * - Space: O(1) constant.
-     * @param {number} num The num to add.
-     */
-    insert(num) {
-      this.heap.push(num);
-      this.shiftUp();
-      // .push on array returns the new length
-      return this.size();
-    }
+  /**
+   * Retrieves the size of the heap, ignoring the null placeholder.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {number}
+   */
+  size() {
+    // - 1 since 0 index is unused
+    return this.heap.length - 1;
+  }
 
-    // AKA: siftUp, heapifyUp, bubbleUp to restore order after insert
-    shiftUp() {
-      let idxOfNodeToShiftUp = this.heap.length - 1;
+  /**
+   * Retrieves the top (minimum number) in the heap without removing it.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {?number} Null if empty.
+   */
+  top() {
+    return this.heap.length > 1 ? this.heap[1] : null;
+  }
 
-      while (idxOfNodeToShiftUp > 1) {
-        const idxOfParent = this.idxOfParent(idxOfNodeToShiftUp);
+  /**
+   * Inserts a new number into the heap and reorders heap to maintain order.
+   * 1. Push new num to back.
+   * 2. Iteratively swap the new num with it's parent while it is smaller than
+   *    it's parent.
+   * - Time: O(log n) logarithmic due to shiftUp.
+   * - Space: O(1) constant.
+   * @param {number} num The num to add.
+   */
+  insert(num) {
+    this.heap.push(num);
+    this.shiftUp();
+    // .push on array returns the new length
+    return this.size();
+  }
 
-        const isParentSmallerOrEqual =
-          this.heap[idxOfParent] <= this.heap[idxOfNodeToShiftUp];
+  // AKA: siftUp, heapifyUp, bubbleUp to restore order after insert
+  shiftUp() {
+    let idxOfNodeToShiftUp = this.heap.length - 1;
 
-        // Parent is supposed to be smaller so ordering is complete.
-        if (isParentSmallerOrEqual) {
-          break;
-        }
+    while (idxOfNodeToShiftUp > 1) {
+      const idxOfParent = this.idxOfParent(idxOfNodeToShiftUp);
 
-        this.swap(idxOfNodeToShiftUp, idxOfParent);
-        // after swapping the node is at idxOfParent now.
-        idxOfNodeToShiftUp = idxOfParent;
+      const isParentSmallerOrEqual =
+        this.heap[idxOfParent] <= this.heap[idxOfNodeToShiftUp];
+
+      // Parent is supposed to be smaller so ordering is complete.
+      if (isParentSmallerOrEqual) {
+        break;
       }
+
+      this.swap(idxOfNodeToShiftUp, idxOfParent);
+      // after swapping the node is at idxOfParent now.
+      idxOfNodeToShiftUp = idxOfParent;
     }
-  
-    /**
-     * Logs the tree horizontally with the root on the left and the index in
-     * parenthesis using reverse inorder traversal.
-     */
-    printHorizontalTree(parentIdx = 1, spaceCnt = 0, spaceIncr = 10) {
-      if (parentIdx > this.heap.length - 1) {
-        return;
-      }
-  
-      spaceCnt += spaceIncr;
-      this.printHorizontalTree(parentIdx * 2 + 1, spaceCnt);
-  
-      console.log(
-        " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
-          `${this.heap[parentIdx]} (${parentIdx})`
-      );
-  
-      this.printHorizontalTree(parentIdx * 2, spaceCnt);
+  }
+
+  /**
+   * Logs the tree horizontally with the root on the left and the index in
+   * parenthesis using reverse inorder traversal.
+   */
+  printHorizontalTree(parentIdx = 1, spaceCnt = 0, spaceIncr = 10) {
+    if (parentIdx > this.heap.length - 1) {
+      return;
     }
 
-      /**
-       * Extracts the min num from the heap and then re-orders the heap to
-       * maintain order so the next min is ready to be extracted.
-       * 1. Save the first node to a temp var.
-       * 2. Pop last node off and set idx1 equal to the popped value.
-       * 3. Iteratively swap the old last node that is now at idx1 with it's
-       *    smallest child IF the smallest child is smaller than it.
-       * - Time: O(log n) logarithmic due to shiftDown.
-       * - Space: O(1) constant.
-       * @returns {?number} The min number or null if empty.
-       */
-      extract() {}
+    spaceCnt += spaceIncr;
+    this.printHorizontalTree(parentIdx * 2 + 1, spaceCnt);
+
+    console.log(
+      " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+        `${this.heap[parentIdx]} (${parentIdx})`
+    );
+
+    this.printHorizontalTree(parentIdx * 2, spaceCnt);
+  }
+
+  /**
+   * Extracts the min num from the heap and then re-orders the heap to
+   * maintain order so the next min is ready to be extracted.
+   * 1. Save the first node to a temp var.
+   * 2. Pop last node off and overwrite idx1 with it.
+   * 3. Iteratively swap the old last node that is now at idx1 with it's
+   *    smallest child IF the smallest child is smaller than it.
+   * - Time: O(log n) logarithmic due to shiftDown.
+   * - Space: O(1) constant.
+   * @returns {?number} The min number or null if empty.
+   */
+  extract() {
+    // nothing to remove
+    if (this.heap.length === 1) {
+      return null;
+    }
+
+    const heap = this.heap;
+    const min = heap[1];
+    const lastNode = heap.pop();
+
+    // last item is being removed, no more work required
+    if (heap.length === 1) {
+      return min;
+    }
+
+    // last node is overwriting the idx 1 to "remove" idx 1
+    heap[1] = lastNode;
+    // since we put the lastNode at the start, it needs to be swapped down to it's correct position to restore the order
+    this.shiftDown();
+    return min;
+  }
+
+  // AKA: siftDown, heapifyDown, bubbleDown, sinkDown to restore order after extracting min
+  shiftDown() {
+    let idxOfNodeToShiftDown = 1;
+    let idxOfLeftChild = this.idxOfLeftChild(idxOfNodeToShiftDown);
+
+    // while there is at least 1 child
+    while (idxOfLeftChild < this.heap.length) {
+      const idxOfRightChild = this.idxOfRightChild(idxOfNodeToShiftDown);
+      let idxOfSmallestChild = idxOfLeftChild;
+      const isRightChildInBounds = idxOfRightChild < this.heap.length;
+
+      // Don't compare right child if it's out of bounds.
+      // less than or greater than undefined has weird behavior (always false).
+      const isRightChildSmaller =
+        isRightChildInBounds &&
+        this.heap[idxOfRightChild] < this.heap[idxOfLeftChild];
+
+      if (isRightChildSmaller) {
+        idxOfSmallestChild = idxOfRightChild;
+      }
+
+      const isParentSmallerOrEqual =
+        this.heap[idxOfNodeToShiftDown] <= this.heap[idxOfSmallestChild];
+
+      // Parent is supposed to be smaller, so ordering is complete.
+      if (isParentSmallerOrEqual) {
+        break;
+      }
+
+      this.swap(idxOfNodeToShiftDown, idxOfSmallestChild);
+
+      // after swapping the node is now at the idxOfSmallestChild.
+      idxOfNodeToShiftDown = idxOfSmallestChild;
+      idxOfLeftChild = this.idxOfLeftChild(idxOfNodeToShiftDown);
+    }
+  }
 }
--- algos/data_structures/SinglyLinkedList/index.js
+++ algos/data_structures/SinglyLinkedList/index.js
@@ -406,36 +406,174 @@
 
     /**
      * Concatenates the nodes of a given list onto the back of this list.
-     * - Time: O(?).
-     * - Space: O(?).
+     * - Time: O(n) n = "this" list length -> O(n) linear.
+     *    addList does not need to be looped over.
+     * - Space: O(1) constant, although this list grows by addList's length,
+     *    our algo doesn't create extra objects or arrays to take up more space.
      * @param {SinglyLinkedList} addList An instance of a different list whose
      *    whose nodes will be added to the back of this list.
      * @returns {SinglyLinkedList} This list with the added nodes.
      */
-    concat(addList) {}
+    concat(addList) {
+      let runner = this.head;
+
+      if (runner === null) {
+        this.head = addList.head;
+      } else {
+        while (runner.next) {
+          runner = runner.next;
+        }
+        runner.next = addList.head;
+      }
+      return this;
+    }
+
+    /**
+     * Finds the node with the smallest number as data and moves it to the front
+     * of this list.
+     * - Time: O(2n) n = list length -> O(n) linear,
+     *    2nd loop could go to end if min is at end.
+     * - Space: O(1) constant.
+     * @returns {SinglyLinkedList} This list.
+     */
+    moveMinFront() {
+      /* 
+        Alternatively, we could swap the data only in min node and head,
+        but it's better to swap the nodes themselves in case anyone has variables
+        pointing to these nodes already so that we don't unexpectedly change the
+        the data in those nodes potentially causing unwanted side-effects.
+      */
+      if (this.isEmpty()) {
+        return this;
+      }
+
+      let minNode = this.head;
+      let runner = this.head;
+      let prev = this.head;
+
+      while (runner) {
+        if (runner.data < minNode.data) {
+          minNode = runner;
+        }
+
+        runner = runner.next;
+      }
+      // now that we know the min, if it is already the head, nothing needs to be done
+      if (minNode === this.head) {
+        return this;
+      }
+
+      runner = this.head;
+
+      while (runner !== minNode) {
+        prev = runner;
+        runner = runner.next;
+      }
+
+      prev.next = minNode.next; // remove the minNode
+      minNode.next = this.head;
+      this.head = minNode;
+      return this;
+    }
 
     /**
      * Finds the node with the smallest data and moves that node to the front of
      * this list.
-     * - Time: O(?).
-     * - Space: O(?).
+     * - Time: O(n) linear, n = list length. This avoids the extra loop in
+     *    the above sln.
+     * - Space: O(n) linear.
      * @returns {SinglyLinkedList} This list.
      */
-    moveMinToFront() {}
+    moveMinToFront() {
+      if (this.isEmpty()) {
+        return this;
+      }
 
-    // EXTRA
+      let minNode = this.head;
+      let runner = this.head;
+      let prev = this.head;
+
+      while (runner.next) {
+        if (runner.next.data < minNode.data) {
+          prev = runner;
+          minNode = runner.next;
+        }
+
+        runner = runner.next;
+      }
+
+      if (minNode === this.head) {
+        return this;
+      }
+
+      prev.next = minNode.next;
+      minNode.next = this.head;
+      this.head = minNode;
+      return this;
+    }
+
     /**
      * Splits this list into two lists where the 2nd list starts with the node
      * that has the given value.
-     * splitOnVal(5) for the list (1=>3=>5=>2=>4) will change list to (1=>3)
+     * splitOnVal(5) for the list (1=>3=>5=>2=>4) will change list to (1=>3),
      * and the return value will be a new list containing (5=>2=>4)
-     * - Time: O(?).
-     * - Space: O(?).
+     * - Time: O(n) linear, n = list length, could split on last node.
+     * - Space: O(1) constant.
      * @param {any} val The value in the node that the list should be split on.
      * @returns {SinglyLinkedList} The split list containing the nodes that are
      *    no longer in this list.
      */
-    splitOnVal(val) {}
+    splitOnVal(val) {
+      const newList = new SinglyLinkedList();
+
+      if (!this.head) {
+        return newList;
+      }
+
+      if (this.head.data === val) {
+        newList.head = this.head;
+        this.head = null;
+        return newList;
+      }
+
+      let runner = this.head;
+
+      while (runner.next) {
+        if (runner.next.data === val) {
+          newList.head = runner.next;
+          runner.next = null;
+          return newList;
+        }
+        runner = runner.next;
+      }
+      return newList;
+    }
+
+    /**
+     * Reverses this list in-place without using any extra lists.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {SinglyLinkedList} This list.
+     */
+    reverse() {}
+
+    /**
+     * Determines whether the list has a loop in it which would result in
+     * infinitely traversing unless otherwise avoided. A loop is when a node's
+     * next points to a node that is behind it.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {boolean} Whether the list has a loop or not.
+     */
+    hasLoop() {}
+
+    /**
+     * Removes all the nodes that have a negative integer as their data.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {SinglyLinkedList} This list after the negatives are removed.
+     */
+    removeNegatives() {}
 }
   
 /******************************************************************* 
