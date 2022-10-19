91d39c213b9df4ee7ab916b77a29d0c3e157cdd3
Max R <max.l.rauchman@gmail.com>
2022-10-19 10:58:50 -0400

    W4D3 Algos

--- algos/data_structures/Queue/index.js
+++ algos/data_structures/Queue/index.js
@@ -15,8 +15,11 @@
      * @param {any} item The new item to add to the back.
      * @returns {number} The new size of this queue.
      */
-    enqueue(item) {}
-  
+    enqueue(item) {
+      this.items.push(item);
+      return this.size();
+    }
+
     /**
      * Removes and returns the first item of this queue.
      * - Time: O(n) linear, due to having to shift all the remaining items to
@@ -24,31 +27,77 @@
      * - Space: O(1) constant.
      * @returns {any} The first item or undefined if empty.
      */
-    dequeue() {}
-  
+    dequeue() {
+      return this.items.shift();
+    }
+
     /**
      * Retrieves the first item without removing it.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {any} The first item or undefined if empty.
      */
-    front() {}
-  
+    front() {
+      return this.items[0];
+    }
+
     /**
      * Returns whether or not this queue is empty.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {boolean}
      */
-    isEmpty() {}
-  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+
     /**
      * Retrieves the size of this queue.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {number} The length.
      */
-    size() {}
+    size() {
+      return this.items.length;
+    }
+
+    /**
+     * Logs the items of this queue.
+     * - Time: O(n) linear.
+     * - Space: O(n) linear.
+     * @returns {string} The same string that is logged.
+     */
+    print() {
+      const str = this.items.join(" ");
+      console.log(str);
+      return str;
+    }
+
+    /**
+     * Compares this queue to another given queue to see if they are equal.
+     * Avoid indexing the queue items directly via bracket notation, use the
+     * queue methods instead for practice.
+     * Use no extra array or objects.
+     * The queues should be returned to their original order when done.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Queue} q2 The queue to be compared against this queue.
+     * @returns {boolean} Whether all the items of the two queues are equal and
+     *    in the same order.
+     */
+    compareQueues(q2) {}
+
+    /**
+     * Determines if the queue is a palindrome (same items forward and backwards).
+     * Avoid indexing queue items directly via bracket notation, instead use the
+     * queue methods for practice.
+     * Use only 1 stack as additional storage, no other arrays or objects.
+     * The queue should be returned to its original order when done.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean}
+     */
+    isPalindrome() {}
 }
   
 /* Rebuild the above class using a linked list instead of an array. */
